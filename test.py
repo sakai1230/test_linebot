b1ac5eab98870733a0e5b657b39eee86289707bf
TempeiSakai <tenten.4649.goodluck@gmail.com>
2022-08-31 16:11:14 +0900

    test.py 変更

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-from linebot.exceptions import LineBotApiError
 
-line_bot_api = LineBotApi('SEJ26ZEb0DM7IUhv/BQNmczqT379XuPxxkUZ0s+bGq5iWKjW7DLihxWyzNerUNGN/PEZfOjeihuxtygdiv3KNuMrpcE1IgMX/XySPckk9oYBcmhS+UY4haRqORZrmW8VbQxKetjR8Xnj0vvuwGDogAdB04t89/1O/w1cDnyilFU=')
+CHANNEL_ACCESS_TOKEN = 'SEJ26ZEb0DM7IUhv/BQNmczqT379XuPxxkUZ0s+bGq5iWKjW7DLihxWyzNerUNGN/PEZfOjeihuxtygdiv3KNuMrpcE1IgMX/XySPckk9oYBcmhS+UY4haRqORZrmW8VbQxKetjR8Xnj0vvuwGDogAdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
-try:
-    line_bot_api.reply_message('<reply_token>', TextSendMessage(text='Hello World!'))
-except LineBotApiError as e:
-    # error handle
-    ...
+def main():
+    USER_ID = 'Uc61807301977299f45ab29dbe3437d5f'
+    messages = TextSendMessage(text='やあ')
+    line_bot_api.push_message(USER_ID, messages)
+
+if __name__ == "__main__":
+    main()
